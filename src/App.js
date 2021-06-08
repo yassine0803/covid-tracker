eb2e209ee97c2ebdd1df3a112943cb02dbec9a3d
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-06-08 01:12:45 +0100

    pass data to cards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import {Cards, Chart, CountryPicker } from './components';
+import { useState,useEffect } from 'react';
 import styles from './App.module.css';
+import {fetchData} from './api';
 
 const App = () => {
+  const [data, setData] = useState()
+  const getData = async () =>{
+    const data = await fetchData();
+    setData(data);
+    console.log(data);
+  }
+  useEffect(() => {
+    getData();
+  }, [])
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <Chart />
       <CountryPicker />
     </div>
